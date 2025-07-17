0a422f92569512a84fae81b22dc448059a9dfe97
manudiv15 <38869988+manudiv16@users.noreply.github.com>
2025-07-17 16:24:01 -0300

    Update version to 0.3.2 and add SHA256 checksums for all platforms

--- Formula/eaws.rb
+++ Formula/eaws.rb
@@ -1,19 +1,19 @@
 class Eaws < Formula
   desc "Simple AWS CLI tool for ECS, CloudWatch, and CodePipeline"
   homepage "https://github.com/manudiv16/eaws"
-  version "0.2.0"
+  version "0.3.2"
   
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/manudiv16/eaws/releases/download/v0.3.2/eaws-darwin-arm64"
-      sha256 "YOUR_ARM64_SHA256_HERE"
+      sha256 "e4ca0519511859f6acd317c5ac73741bf576fd6d580e32340583ce8a58494fc6"
     else
       url "https://github.com/manudiv16/eaws/releases/download/v0.3.2/eaws-darwin-amd64"
-      sha256 "YOUR_AMD64_SHA256_HERE"
+      sha256 "c6d54584211a942374235ca04aa15880db24e56e821abb367a50df5d5d2ade2a"
     end
   elsif OS.linux?
     url "https://github.com/manudiv16/eaws/releases/download/v0.3.2/eaws-linux-amd64"
-    sha256 "YOUR_LINUX_SHA256_HERE"
+    sha256 "2c03c5a2f311b415428a80bc7a3e1cde937178010c121b3af072e78874aae172"
   end
 
   depends_on "awscli"
